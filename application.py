0f29c081ad1418bd554a779944cd2a33bdcd0583
Marvin Largaespada <59581051+ShadowfallES@users.noreply.github.com>
2022-04-13 22:12:05 -0600

    Update application.py

--- application.py
+++ application.py
@@ -18,8 +18,8 @@ import json
 app = Flask(__name__)
 
 # Check for environment variable
-if not os.getenv("DBA_URL"):
-    raise RuntimeError("DBA_URL is not set")
+if not os.getenv("DATABASE"):
+    raise RuntimeError("DATABASE is not set")
 
 # Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
@@ -27,7 +27,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Set up database
-engine = create_engine(os.getenv("DBA_URL"))
+engine = create_engine(os.getenv("DATABASE"))
 db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
